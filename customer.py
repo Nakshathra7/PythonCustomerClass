454eaa393214bdfbbf3f4fd9b7d16074de3aebbd
Nakshathra7 <anumano92@gmail.com>
2020-03-03 11:03:55 -0500

    Update customer.py

--- customer.py
+++ customer.py
@@ -6,16 +6,35 @@ class customerList:
 		self.data = []
 		self.tempdata = {}
 		self.tn = 'customers'
-		self.fn1 = ['fname', 'lname', 'email', 'password', 'subscribed']
+		self.fn1 = []
+		#self.fn1 = ['fname', 'lname', 'email', 'password', 'subscribed']
 		self.errList = []
 		self.conn = None
 		self.regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+		self.pk = 'id'
+		self.getFields()
 
 	def connect(self):
 		#return pymysql.connect(host='',port=3306,user='',password='',db='',autocommit=True)
 		import config
 		self.conn = pymysql.connect(host=config.DB['host'],port=config.DB['port'],user=config.DB['user'],password=config.DB['password'],db=config.DB['db'],autocommit=True)
 		#self.conn = pymysql.connect(host='',port=3306,user='',password='',db='',autocommit=True)
+
+	def getFields(self):
+		sql = 'DESCRIBE `' + self.tn +'`;'
+		self.connect()
+		cur = self.conn.cursor(pymysql.cursors.DictCursor)
+		cur.execute(sql)
+		self.fn1 = []
+		for row in cur:
+			#self.data.append(row)
+			if row["Key"] != "PRI":
+				self.fn1.append(row['Field'])
+			else:
+				self.pk = row["Field"]
+
+		#print(row)
+		print(self.fn1)
 
 	def add(self):
 		#if len(self.tempdata) == len(self.fn1):
@@ -73,10 +92,17 @@ class customerList:
 		cols = '`,`'.join(self.fn1)
 		cols = '`'+cols+'`'
 		vals = ('%s,'*len(self.fn1))[:-1]
+		#cols = ''
+		#vals = ''
 		tokens = []
 		for fieldname in self.fn1:
-			tokens.append(self.data[n][fieldname])
-
+			#if fieldname in self.data[n].keys():
+				tokens.append(self.data[n][fieldname])
+				#vals += '%s,'
+				#cols += '`'+fieldname+'`,'
+		#vals = vals[:-1]
+		#cols = cols[:-1]
+		print(cols)
 		sql='INSERT INTO `' + self.tn + '` (' +cols+ ') VALUES (' + vals +');'
 		#conn = self.connect()
 		self.connect()
@@ -85,7 +111,80 @@ class customerList:
 		print(sql)
 		print(tokens)
 		cur.execute(sql,tokens)
+		self.data[n][self.pk] = cur.lastrowid
 
+	def delete(self,n=0):
+		item = self.data.pop(n)
+		self.deleteByID(item[self.pk])
+
+	def deleteByID(self,id):
+		sql = 'DELETE FROM `' + self.tn +'` WHERE `'+self.pk+'` = %s;' 
+		tokens = (id)
+		self.connect()
+		cur = self.conn.cursor(pymysql.cursors.DictCursor)
+		cur.execute(sql,tokens)
+
+	def getByID(self,id):
+		sql = 'SELECT * FROM `' + self.tn +'` WHERE `'+self.pk+'` = %s;' 
+		tokens = (id)
+		self.connect()
+		cur = self.conn.cursor(pymysql.cursors.DictCursor)
+		cur.execute(sql,tokens)
+		self.data = []
+		for row in cur:
+			self.data.append(row)
+
+	def getAll(self,order=None):
+		sql = 'SELECT * FROM `' + self.tn +'`'
+		if order != None:
+			sql += ' ORDER BY `'+order+'`'
+		self.connect()
+		cur = self.conn.cursor(pymysql.cursors.DictCursor)
+		cur.execute(sql)
+		self.data = []
+		for row in cur:
+			self.data.append(row)
+
+	def update(self, n=0):
+		
+		tokens = []
+		setstring = ''
+		for fieldname in self.data[n].keys():
+			if fieldname != self.pk:
+				setstring += ' `'+fieldname+'` = %s,'
+				tokens.append(self.data[n][fieldname])
+
+		setstring = setstring[:-1]
+
+		sql='UPDATE `' + self.tn + '` SET ' + setstring + ' WHERE `' + self.pk + '`=%s'
+		tokens.append(self.data[n][self.pk])
+		#conn = self.connect()
+		self.connect()
+		print(sql)
+		#cur = conn.cursor(pymysql.cursors.DictCursor)
+		cur = self.conn.cursor(pymysql.cursors.DictCursor)
+		cur.execute(sql,tokens)
+		#self.data[n][self.pk] = cur.lastrowid
+
+	def getByField(self,field,value):
+		sql = 'SELECT * FROM `' + self.tn +'` WHERE `'+field+'` = %s;' 
+		tokens = (value)
+		self.connect()
+		cur = self.conn.cursor(pymysql.cursors.DictCursor)
+		cur.execute(sql,tokens)
+		self.data = []
+		for row in cur:
+			self.data.append(row)
+
+	def getLikeField(self,field,value):
+		sql = 'SELECT * FROM `' + self.tn +'` WHERE `'+field+'` LIKE %s;' 
+		tokens = ('%' + value + '%')
+		self.connect()
+		cur = self.conn.cursor(pymysql.cursors.DictCursor)
+		cur.execute(sql,tokens)
+		self.data = []
+		for row in cur:
+			self.data.append(row)
 
 
 
